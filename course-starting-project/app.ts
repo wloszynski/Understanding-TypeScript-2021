03b28350e58e5d26a7264635669b43620c526521
wloszynski <adrian@wloszynski.pl>
2021-01-18 14:48:04 +0100

    enum

--- course-starting-project/app.ts
+++ course-starting-project/app.ts
@@ -6,22 +6,38 @@
 //   age: 20,
 // };
 
-const person: {
-  name: string;
-  age: number;
-  hobbies: string[];
-  role: [number, string];
-} = {
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   role: [number, string];
+// } = {
+//   name: "Adrian",
+//   age: 20,
+//   hobbies: ["Sports", "Cooking"],
+//   role: [2, "author"],
+// };
+
+// const ADMIN = 0;
+// const READ_ONLY = 1;
+// const AUTHOR = 2;
+
+enum Role {
+  ADMIN,
+  READ_ONLY,
+  AUTHOR,
+}
+
+const person = {
   name: "Adrian",
   age: 20,
   hobbies: ["Sports", "Cooking"],
-  role: [2, "author"],
+  role: Role.ADMIN,
 };
 
 // person.role.push("admin");
 // person.role[1] = 10;
-
-person.role = [0, "admin"];
+// person.role = [0, "admin"];
 
 let favouriteActivities: string[];
 favouriteActivities = ["Sports"];
@@ -30,4 +46,8 @@ console.log(person.name);
 
 for (const hobby of person.hobbies) {
   console.log(hobby.toUpperCase());
+}
+
+if (person.role === Role.ADMIN) {
+  console.log(Role.ADMIN);
 }
