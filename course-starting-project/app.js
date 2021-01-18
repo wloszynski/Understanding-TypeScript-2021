2234561f12b8a5ebe907e6aadac03041cb31c718
wloszynski <adrian@wloszynski.pl>
2021-01-18 14:42:26 +0100

    working with enums

--- course-starting-project/app.js
+++ course-starting-project/app.js
@@ -8,8 +8,11 @@
 var person = {
     name: "Adrian",
     age: 20,
-    hobbies: ["Sports", "Cooking"]
+    hobbies: ["Sports", "Cooking"],
+    role: [2, "author"]
 };
+person.role.push("admin");
+person.role[1] = 10;
 var favouriteActivities;
 favouriteActivities = ["Sports"];
 console.log(person.name);
--- course-starting-project/app.ts
+++ course-starting-project/app.ts
@@ -6,11 +6,22 @@
 //   age: 20,
 // };
 
-const person = {
+const person: {
+  name: string;
+  age: number;
+  hobbies: string[];
+  role: [number, string];
+} = {
   name: "Adrian",
   age: 20,
   hobbies: ["Sports", "Cooking"],
+  role: [2, "author"],
 };
+
+// person.role.push("admin");
+// person.role[1] = 10;
+
+person.role = [0, "admin"];
 
 let favouriteActivities: string[];
 favouriteActivities = ["Sports"];
