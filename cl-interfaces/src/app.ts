66e9b5eca31684754361d0de063035d5843ac2b8
wloszynski <adrian@wloszynski.pl>
2021-01-19 16:39:32 +0100

    'private' and 'public' access modifiers

--- cl-interfaces/src/app.ts
+++ cl-interfaces/src/app.ts
@@ -1,5 +1,6 @@
 class Department {
   name: string;
+  private employees: string[] = [];
 
   constructor(n: string) {
     this.name = n;
@@ -8,12 +9,27 @@ class Department {
   describe(this: Department) {
     console.log("Department: " + this.name);
   }
+
+  addEmployee(employee: string) {
+    this.employees.push(employee);
+  }
+
+  printEmployeeInformation() {
+    console.log(this.employees.length);
+    console.log(this.employees);
+  }
 }
 
 const accounting = new Department("Accounting");
+
+accounting.addEmployee("Tim");
+accounting.addEmployee("Tom");
+
+accounting.printEmployeeInformation();
+
 console.log(accounting);
 accounting.describe();
 
-const accountingCopy = { name: "s", describe: accounting.describe };
+// const accountingCopy = { name: "s", describe: accounting.describe };
 
-accountingCopy.describe();
+// accountingCopy.describe();
