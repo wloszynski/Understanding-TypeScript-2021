2eb68275ae235763c2329e51b2a2b2668303df2a
wloszynski <adrian@wloszynski.pl>
2021-01-19 16:24:41 +0100

    constructor functions

--- cl-interfaces/src/app.ts
+++ cl-interfaces/src/app.ts
@@ -1,1 +1,19 @@
-// Code goes here!
+class Department {
+  name: string;
+
+  constructor(n: string) {
+    this.name = n;
+  }
+
+  describe(this: Department) {
+    console.log("Department: " + this.name);
+  }
+}
+
+const accounting = new Department("Accounting");
+console.log(accounting);
+accounting.describe();
+
+const accountingCopy = { name: "s", describe: accounting.describe };
+
+accountingCopy.describe();
