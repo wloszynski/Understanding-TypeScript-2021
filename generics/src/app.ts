7bdf1ee5573ab020c740995be4da326730547820
wloszynski <adrian@wloszynski.pl>
2021-01-20 15:45:31 +0100

    utility types

--- generics/src/app.ts
+++ generics/src/app.ts
@@ -86,3 +86,24 @@ const numberStorage = new DataStorage<number>();
 
 // objStorage.removeItem({ name: "Tim" });
 // console.log(objStorage.getItems());
+
+interface CourseGoal {
+  title: string;
+  description: string;
+  completeUntil: Date;
+}
+
+function createCourseGoal(
+  title: string,
+  description: string,
+  date: Date
+): CourseGoal {
+  let courseGoal: Partial<CourseGoal> = {};
+  courseGoal.title = title;
+  courseGoal.description = description;
+  courseGoal.completeUntil = date;
+  return courseGoal as CourseGoal;
+}
+
+const names: Readonly<string[]> = ["Tim", "Tom"];
+names.push("Tomas");
