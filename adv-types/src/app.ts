b1c0ca16f98d6c34de2c1e4ca7958cbd0a2255b9
wloszynski <adrian@wloszynski.pl>
2021-01-20 10:35:22 +0100

    intersection types

--- adv-types/src/app.ts
+++ adv-types/src/app.ts
@@ -1,1 +1,25 @@
-// Code goes here!
+type Admin = {
+  name: string;
+  priviliges: string[];
+};
+
+type Employee = {
+  name: string;
+  startDate: Date;
+};
+
+// interface ElevatedEmployee extends Employee, Admin{}
+
+type ElevatedEmployee = Admin & Employee;
+
+const e1: ElevatedEmployee = {
+  name: "Tim",
+  priviliges: ["create-server"],
+  startDate: new Date(),
+};
+
+type Combinable = string | number;
+type Numeric = number | boolean;
+
+// gonna be type: number
+type Universal = Combinable & Numeric;
