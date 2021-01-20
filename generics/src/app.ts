30d8cee77526f339098890e420992f9eee51c38c
wloszynski <adrian@wloszynski.pl>
2021-01-20 14:59:24 +0100

    generic function

--- generics/src/app.ts
+++ generics/src/app.ts
@@ -1,1 +1,25 @@
-// Code goes here!
+// const names = ["Tim", "Tom"];
+
+// const names: any[] = [];
+// const names: Array<string> = ["Tim", "Tom"]; // string[]
+// names[0].split("");
+
+// const promise: Promise<string> = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve("This is done!");
+//   }, 2000);
+// });
+
+// promise.then((data) => {
+//   data.split(" ");
+// });
+
+function merge<T, U>(objA: T, objB: U) {
+  return Object.assign(objA, objB);
+}
+
+// console.log(merge({ name: "Tim" }, { age: 20 }));
+const mergedObj = merge({ name: "Tim" }, { age: 20 });
+const mergedObj2 = merge({ name: "Tim", hobbies: ["Football"] }, { age: 20 });
+mergedObj.age;
+mergedObj2.age;
