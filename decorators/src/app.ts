f831bd27153de3716870ab8166f248ce602203bb
wloszynski <adrian@wloszynski.pl>
2021-01-21 14:39:53 +0100

    multiple decorators

--- decorators/src/app.ts
+++ decorators/src/app.ts
@@ -4,6 +4,8 @@
 // }
 
 function Logger(logString: string) {
+  console.log("LOGGER FACTORY");
+
   return function (constructor: Function) {
     console.log(logString);
     console.log(constructor);
@@ -11,7 +13,10 @@ function Logger(logString: string) {
 }
 
 function WithTemplate(template: string, hookId: string) {
+  console.log("TEMPLATE FACTORY");
+
   return function (constructor: any) {
+    console.log("Rendering template");
     const hookEl = document.getElementById(hookId);
     const p = new constructor();
     if (hookEl) {
@@ -22,6 +27,8 @@ function WithTemplate(template: string, hookId: string) {
 }
 
 // @Logger("LOGGING - PERSON")
+
+@Logger("Logging")
 @WithTemplate("<h1>My Person Object</h1>", "app")
 class Person {
   name = "Tim";
