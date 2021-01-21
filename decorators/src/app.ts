d9aee5cbc6bc9f47a619017e84d6cab73b5b5d03
wloszynski <adrian@wloszynski.pl>
2021-01-21 15:25:06 +0100

    autobind decorator

--- decorators/src/app.ts
+++ decorators/src/app.ts
@@ -44,8 +44,8 @@ class Person {
   }
 }
 
-// const pers = new Person();
-// console.log(pers);
+const pers = new Person();
+console.log(pers);
 
 // ---
 
@@ -106,3 +106,33 @@ class Product {
 
 const p1 = new Product("book1", 1);
 const p2 = new Product("book2", 2);
+
+function Autobind(
+  _: any,
+  _2: string | Symbol | number,
+  descriptor: PropertyDescriptor
+) {
+  const originalMethod = descriptor.value;
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    enumerable: false,
+    get() {
+      const boundFn = originalMethod.bind(this);
+      return boundFn;
+    },
+  };
+  return adjDescriptor;
+}
+class Printer {
+  message = "This works!";
+
+  @Autobind
+  showMessage() {
+    console.log(this.message);
+  }
+}
+
+const p = new Printer();
+
+const button = document.querySelector("button")!;
+button.addEventListener("click", p.showMessage);
