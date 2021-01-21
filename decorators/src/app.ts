9a2444bd2a077ecd17494693ae42a299348ad4a7
wloszynski <adrian@wloszynski.pl>
2021-01-21 16:28:39 +0100

    fixing a validator bug

--- decorators/src/app.ts
+++ decorators/src/app.ts
@@ -148,17 +148,22 @@ const registeredValidators: ValidatorConfig = {};
 function Required(target: any, propName: string) {
   registeredValidators[target.constructor.name] = {
     ...registeredValidators[target.constructor.name],
-    [propName]: ["required"],
+    [propName]: [
+      ...registeredValidators[target.constructor.name][propName],
+      "required",
+    ],
   };
 }
 
 function PositiveNumber(target: any, propName: string) {
   registeredValidators[target.constructor.name] = {
     ...registeredValidators[target.constructor.name],
-    [propName]: ["positive"],
+    [propName]: [
+      ...registeredValidators[target.constructor.name][propName],
+      "positive",
+    ],
   };
 }
-
 function validate(obj: any) {
   const objValidatorConfig = registeredValidators[obj.constructor.name];
   if (!objValidatorConfig) {
