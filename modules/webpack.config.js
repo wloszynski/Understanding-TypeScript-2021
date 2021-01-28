63f5bfc1bed316c628e207e50815ba90d78e1a68
wloszynski <adrian@wloszynski.pl>
2021-01-28 17:03:27 +0100

    adding ts loader

--- modules/webpack.config.js
+++ modules/webpack.config.js
@@ -5,5 +5,19 @@ module.exports = {
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
+    publicPath: "dist",
+  },
+  devtool: "inline-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
   },
 };
